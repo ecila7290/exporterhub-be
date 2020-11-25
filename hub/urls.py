3031da3e84e0dcbdbdfd9cb9369173927ea1db36
ecila7290 <jihoon.kim89@gmail.com>
2020-11-25 15:41:30 +0900

    Add: added detail page view and repository view's get/delete method

--- hub/urls.py
+++ hub/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import MainView, CategoryView, RepositoryView
+from .views import MainView, CategoryView, RepositoryView, DetailView
 
 urlpatterns = [
     path('', MainView.as_view()),
-    path('category', CategoryView.as_view()),
+    path('categories', CategoryView.as_view()),
     path('exporter', RepositoryView.as_view()),
+    path('exporter/<int:exporter_id>', DetailView.as_view()),
 ]
