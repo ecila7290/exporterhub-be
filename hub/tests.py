90367345af1a1e103eeb1ca41f4693e586db0890
ecila7290 <jihoon.kim89@gmail.com>
2020-11-27 10:57:19 +0900

    Add: Added unit test for category view

--- hub/tests.py
+++ hub/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase, Client
+
+from .models import Category
+
+client=Client()
+
+class CategoryTest(TestCase):
+    def setUp(self):
+        Category.objects.create(
+            id=1,
+            name='category1'
+        )
+    
+    def tearDown(self):
+        Category.objects.all().delete()
+
+    def test_get_category_success(self):
+        response=client.get('/categories')
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.json(),
+            {
+                'categories':[{
+                'category_id':1,
+                'category_name':'category1'}]
+            }
+        )
+
+    def test_get_category_not_found(self):
+        response=client.get('/category')
+        self.assertEqual(response.status_code,404)
